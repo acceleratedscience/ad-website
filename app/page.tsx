9a92da9dbffdaa3a87a63c75d5926976adfd8770
Ray Lopez <ray.lopez@us.ibm.com>
2024-03-27 17:54:43 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,25 +5,6 @@ import Banner from './components/Banner';
 import ProjectBox from './components/ProjectBox';
 import HeroBanner from './components/HeroBanner';
 import TileBoxes from './components/TileBoxes';
-import { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
-
-export default function Document() {
-  return (
-    <Html>
-      <Head>
-        <script type="text/javascript">
-          window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};
-          heap.load("757684585");
-        </script>
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
-}
 
 export const metadata: Metadata = {
   title: homePageData.seo.title,
