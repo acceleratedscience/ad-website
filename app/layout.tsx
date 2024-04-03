01173eee058fc13928de7e49952528d2f7e0b943
Ray Lopez <ray.lopez@us.ibm.com>
2024-04-03 14:35:40 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import Header from './components/Header';
 import './styles/globals.scss';
 import { Inter } from 'next/font/google';
-import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] });
 const isProduction = process.env.isProduction === 'true';
@@ -17,7 +16,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <head><script type="text/javascript">window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};heap.load("757684585");</script></head>
       <body className={inter.className}>
         <Header />
         <div className='cds--content'>{children}</div>
