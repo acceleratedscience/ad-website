d90e3416ecb5954f441e252bbba0761f4014c0e3
Ray Lopez <ray.lopez@us.ibm.com>
2024-04-03 15:03:04 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import Header from './components/Header';
 import './styles/globals.scss';
 import { Inter } from 'next/font/google';
-import Script from 'next/script'
+import Script from 'next/script';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 const isProduction = process.env.isProduction === 'true';
@@ -17,18 +18,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
+      <head>
+        <Script src="./heap.js"></Script>
+      </head>
       <body className={inter.className}>
         <Header />
         <div className='cds--content'>{children}</div>
       </body>
     </html>
   );
-}
-
-export function Heap() {
-  return (
-    <head>
-      <Script src="./heap.js"></Script>
-    </head>
-  )
 }
