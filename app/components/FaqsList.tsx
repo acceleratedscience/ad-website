708a666de2e002de8a4f749f160ca5159617623d
Moenen Erbuer <moenen.erbuer@ibm.com>
2025-03-25 10:44:43 +0200

    Dynamic lottie import

--- app/components/FaqsList.tsx
+++ app/components/FaqsList.tsx
@@ -1,10 +1,21 @@
-'use client';
-import { Tile, Heading } from '@carbon/react';
-import { Faqs } from '../interfaces';
-import { useState } from 'react';
-import { Player } from '@lottiefiles/react-lottie-player';
-import SectionContainer from './SectionContainer';
-import LinkButton from './LinkButton';
+'use client'
+import { Tile, Heading } from '@carbon/react'
+import { Faqs } from '../interfaces'
+import { useState } from 'react'
+import SectionContainer from './SectionContainer'
+import LinkButton from './LinkButton'
+import dynamic from 'next/dynamic'
+
+// Dynamic import
+// - - -
+// The lottie player refers to 'document', which breaks the SSR build process.
+// Because we have { output: 'export' } in next.config.js, every page is
+// pre-rendered on the server regardless of the 'use client' directive on top.
+// import { Player } from '@lottiefiles/react-lottie-player';
+const Player = dynamic(
+	() => import('@lottiefiles/react-lottie-player').then((mod) => mod.Player),
+	{ ssr: false }
+)
 
 /**
  * @param faqs Faqs
@@ -12,80 +23,84 @@ import LinkButton from './LinkButton';
  * @returns
  */
 export default function FaqsList({
-  faqs,
-  level = 2
+	faqs,
+	level = 2
 }: {
-  faqs: Faqs;
-  level?: any;
+	faqs: Faqs
+	level?: any
 }) {
-  const [currentFaq, setCurrentFaq]: any = useState(0);
+	const [currentFaq, setCurrentFaq]: any = useState(0)
 
-  return (
-    <SectionContainer level={level}>
-      <div className='section-row-columns'>
-        <div className='section-row-col-large'>
-          <Tile className='tile--blue-60'>
-            <Heading className='box-title'>{faqs.title}</Heading>
-            <p
-              className='mt-10 xl:mt-12 mb-8 md: mb-12 xl:mb-20 cds--type-sans text-lg'
-              dangerouslySetInnerHTML={{ __html: faqs.description }}
-            ></p>
+	return (
+		<SectionContainer level={level}>
+			<div className='section-row-columns'>
+				<div className='section-row-col-large'>
+					<Tile className='tile--blue-60'>
+						<Heading className='box-title'>{faqs.title}</Heading>
+						<p
+							className='mt-10 xl:mt-12 mb-8 md: mb-12 xl:mb-20 cds--type-sans text-lg'
+							dangerouslySetInnerHTML={{
+								__html: faqs.description
+							}}
+						></p>
 
-            <div className='w-full flex flex-row overflow-x-scroll md:flex-col md:overflow-hidden snap-x md:snap-none'>
-              {faqs.items.map((faq, index) => (
-                <div
-                  key={index}
-                  className='min-w-[80%] w-[80%] snap-start md:snap-none border-l md:border-l-0 md:border-t border-solid border-white pl-2 md:pl-0 py-2 min-h-[7rem] cds--type-sans'
-                  onMouseEnter={() => setCurrentFaq(index)}
-                >
-                  <span
-                    className={`cursor-pointer text-lg font-semibold ${
-                      currentFaq === index ? 'text-white' : 'cds-text-blue-40'
-                    }`}
-                  >
-                    {faq.title}
-                  </span>
-                  <p
-                    className={`${
-                      index === currentFaq
-                        ? 'max-h-max block'
-                        : 'max-h-0 hidden'
-                    }`}
-                  >
-                    {faq.description}
-                  </p>
-                </div>
-              ))}
-            </div>
-          </Tile>
-        </div>
-        <div className='section-row-col-small'>
-          <Tile className='tile--blue-70 h-full !flex items-end justify-center'>
-            {faqs.items.map(
-              (faq, index) =>
-                currentFaq === index && (
-                  <Player
-                    key={index}
-                    autoplay
-                    loop
-                    src={faq.lottieUrl}
-                    className='w-full'
-                  ></Player>
-                )
-            )}
-          </Tile>
-        </div>
-      </div>
-      {faqs?.url && (
-        <div className='mt-4'>
-          <LinkButton
-            href={faqs.url.href}
-            className='button--yellow !w-full !max-w-none md:!max-w-xs'
-          >
-            {faqs.url.label}
-          </LinkButton>
-        </div>
-      )}
-    </SectionContainer>
-  );
+						<div className='w-full flex flex-row overflow-x-scroll md:flex-col md:overflow-hidden snap-x md:snap-none'>
+							{faqs.items.map((faq, index) => (
+								<div
+									key={index}
+									className='min-w-[80%] w-[80%] snap-start md:snap-none border-l md:border-l-0 md:border-t border-solid border-white pl-2 md:pl-0 py-2 min-h-[7rem] cds--type-sans'
+									onMouseEnter={() => setCurrentFaq(index)}
+								>
+									<span
+										className={`cursor-pointer text-lg font-semibold ${
+											currentFaq === index
+												? 'text-white'
+												: 'cds-text-blue-40'
+										}`}
+									>
+										{faq.title}
+									</span>
+									<p
+										className={`${
+											index === currentFaq
+												? 'max-h-max block'
+												: 'max-h-0 hidden'
+										}`}
+									>
+										{faq.description}
+									</p>
+								</div>
+							))}
+						</div>
+					</Tile>
+				</div>
+				<div className='section-row-col-small'>
+					<Tile className='tile--blue-70 h-full !flex items-end justify-center'>
+						{faqs.items.map(
+							(faq, index) =>
+								currentFaq === index && (
+									<Player
+										key={index}
+										autoplay
+										loop
+										src={faq.lottieUrl}
+										className='w-full'
+									></Player>
+								)
+						)}
+					</Tile>
+				</div>
+			</div>
+			{faqs?.url && (
+				<div className='mt-4'>
+					<LinkButton
+						href={faqs.url.href}
+						className='button--yellow !w-full !max-w-none md:!max-w-xs'
+					>
+						{faqs.url.label}
+					</LinkButton>
+				</div>
+			)}
+		</SectionContainer>
+	)
 }
--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,130 +1,132 @@
-import fs from 'fs';
-import path from 'path';
-import { Metadata } from 'next';
-import ExportedImage from 'next-image-export-optimizer';
-import ArticleList from '@/app/components/ArticleList';
-import FaqsList from '@/app/components/FaqsList';
-import FeaturesBox from '@/app/components/FeaturesBox';
-import ProjectLink from '@/app/components/ProjectLink';
-import TitleBoxText from '@/app/components/TitleBoxText';
-import LinkButton from '@/app/components/LinkButton';
-import ProductPageBannerHero from '@/app/components/ProductPageBannerHero';
-import ImageFullScreen from '@/app/components/ImageFullScreen';
-import { Project } from '@/app/interfaces';
-import TileBoxes from '@/app/components/TileBoxes';
+import fs from 'fs'
+import path from 'path'
+import { Metadata } from 'next'
+import ExportedImage from 'next-image-export-optimizer'
+import ArticleList from '@/app/components/ArticleList'
+import FaqsList from '@/app/components/FaqsList'
+import FeaturesBox from '@/app/components/FeaturesBox'
+import ProjectLink from '@/app/components/ProjectLink'
+import TitleBoxText from '@/app/components/TitleBoxText'
+import LinkButton from '@/app/components/LinkButton'
+import ProductPageBannerHero from '@/app/components/ProductPageBannerHero'
+import ImageFullScreen from '@/app/components/ImageFullScreen'
+import { Project } from '@/app/interfaces'
+import TileBoxes from '@/app/components/TileBoxes'
 
 export const generateStaticParams = async () => {
-  const dir = path.join(process.cwd(), 'public', 'data', 'projects');
-  const filenames = fs.readdirSync(dir);
-  return filenames.map((filename) => ({
-    slug: filename.replace('.json', '')
-  }));
-};
+	const dir = path.join(process.cwd(), 'public', 'data', 'projects')
+	const filenames = fs.readdirSync(dir)
+	return filenames.map((filename) => ({
+		slug: filename.replace('.json', '')
+	}))
+}
 
 type Props = {
-  params: { slug: string };
-};
+	params: { slug: string }
+}
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  // Get project data
-  const project: Project = await import(
-    '@/public/data/projects/' + params.slug + '.json'
-  );
+	// Get project data
+	const project: Project = await import(
+		'@/public/data/projects/' + params.slug + '.json'
+	)
 
-  return {
-    title: project.seo.title,
-    description: project.seo.description,
-    keywords: project.seo.keywords
-  };
+	return {
+		title: project.seo.title,
+		description: project.seo.description,
+		keywords: project.seo.keywords
+	}
 }
 
 export default async function Project({
-  params
+	params
 }: {
-  params: { slug: string };
+	params: { slug: string }
 }) {
-  const project: Project = await import(
-    '@/public/data/projects/' + params.slug + '.json'
-  );
+	const project: Project = await import(
+		'@/public/data/projects/' + params.slug + '.json'
+	)
 
-  return (
-    <div className={`min-h-screen w-full`}>
-      <div
-        className={`background-product-page bg-[image:var(--image-url)] border-8 border-orange-500 bg-cover bg-center`}
-        style={{
-          ['--image-url' as any]: `linear-gradient(to bottom, rgba(255,255,255,0) 50%, #f4f4f4 80%), url(/images/projects/${project.bannerImage.imagePath})`
-        }}
-      ></div>
+	return (
+		<div className={`min-h-screen w-full`}>
+			<div
+				className={`background-product-page bg-[image:var(--image-url)] border-8 border-orange-500 bg-cover bg-center`}
+				style={{
+					['--image-url' as any]: `linear-gradient(to bottom, rgba(255,255,255,0) 50%, #f4f4f4 80%), url(/images/projects/${project.bannerImage.imagePath})`
+				}}
+			></div>
 
-      <div className='md:hidden relative w-full h-[60vh]'>
-        <div
-          className='absolute inset-0 z-20'
-          style={{
-            background: `linear-gradient(to top, rgba(255,255,255,0) 50%, rgba(255,255,255,1) 90%)`
-          }}
-        ></div>
-        <ExportedImage
-          src={`/images/projects/${project.bannerImageMobile.imagePath}`}
-          fill
-          className='object-cover z-10'
-          alt={project.title}
-          loading={'eager'}
-        />
-      </div>
+			<div className='md:hidden relative w-full h-[60vh]'>
+				<div
+					className='absolute inset-0 z-20'
+					style={{
+						background: `linear-gradient(to top, rgba(255,255,255,0) 50%, rgba(255,255,255,1) 90%)`
+					}}
+				></div>
+				<ExportedImage
+					src={`/images/projects/${project.bannerImageMobile.imagePath}`}
+					fill
+					className='object-cover z-10'
+					alt={project.title}
+					loading={'eager'}
+				/>
+			</div>
 
-      <ProductPageBannerHero {...project}>
-        {project.bannerCta && project.bannerCta.href && (
-          <LinkButton
-            href={project.bannerCta.href}
-            className='button--green !w-full !max-w-none md:!max-w-xs'
-          >
-            {project.bannerCta.label}
-          </LinkButton>
-        )}
-      </ProductPageBannerHero>
+			<ProductPageBannerHero {...project}>
+				{project.bannerCta && project.bannerCta.href && (
+					<LinkButton
+						href={project.bannerCta.href}
+						className='button--green !w-full !max-w-none md:!max-w-xs'
+					>
+						{project.bannerCta.label}
+					</LinkButton>
+				)}
+			</ProductPageBannerHero>
 
-      {project.mainImage && project.mainImageMobile && (
-        <ImageFullScreen
-          image={project.mainImage}
-          imageMobile={project.mainImageMobile}
-          loading='lazy'
-        ></ImageFullScreen>
-      )}
+			{project.mainImage && project.mainImageMobile && (
+				<ImageFullScreen
+					image={project.mainImage}
+					imageMobile={project.mainImageMobile}
+					loading='lazy'
+				></ImageFullScreen>
+			)}
 
-      {project.titleBoxText && <TitleBoxText text={project.titleBoxText} />}
+			{project.titleBoxText && (
+				<TitleBoxText text={project.titleBoxText} />
+			)}
 
-      {project.faqs && <FaqsList faqs={project.faqs}></FaqsList>}
+			{project.faqs && <FaqsList faqs={project.faqs}></FaqsList>}
 
-      {project.articles && (
-        <ArticleList articles={project.articles}>
-          <div className='mt-8'>
-            <LinkButton
-              className='!w-full !max-w-none md:!max-w-xs'
-              href={project.articles.url.href}
-            >
-              {project.articles.url.label}
-            </LinkButton>
-          </div>
-        </ArticleList>
-      )}
+			{project.articles && (
+				<ArticleList articles={project.articles}>
+					<div className='mt-8'>
+						<LinkButton
+							className='!w-full !max-w-none md:!max-w-xs'
+							href={project.articles.url.href}
+						>
+							{project.articles.url.label}
+						</LinkButton>
+					</div>
+				</ArticleList>
+			)}
 
-      {project.features && (
-        <FeaturesBox features={project.features}></FeaturesBox>
-      )}
+			{project.features && (
+				<FeaturesBox features={project.features}></FeaturesBox>
+			)}
 
-      {project.pricing && (
-        <TileBoxes
-          tileBoxes={project.pricing}
-          className='mt-16 md:mt-24'
-        ></TileBoxes>
-      )}
+			{project.pricing && (
+				<TileBoxes
+					tileBoxes={project.pricing}
+					className='mt-16 md:mt-24'
+				></TileBoxes>
+			)}
 
-      {project.tryItOut && (
-        <ProjectLink
-          label={project.tryItOut.label}
-          href={project.tryItOut.href}
-        ></ProjectLink>
-      )}
-    </div>
-  );
+			{project.tryItOut && (
+				<ProjectLink
+					label={project.tryItOut.label}
+					href={project.tryItOut.href}
+				></ProjectLink>
+			)}
+		</div>
+	)
 }
